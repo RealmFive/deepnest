369ef4f2697f289e2e135af9339fb2892b8e9df2
Mattie Hansen <mattie.hansen@realmfive.com>
2023-02-07 14:43:14 -0600

    Add recursive methods

--- lib/deep_nest.rb
+++ lib/deep_nest.rb
@@ -1,16 +1,58 @@
 # frozen_string_literal: true
 
-class DeepNest
+module DeepNest
   class << self
-    def deep_dup(structure)
-      case structure
+    def deep_dup(obj)
+      case obj
       when Array
-        structure.map { |x| deep_dup(x) }
+        obj.map { |x| deep_dup(x) }
       when Hash
-        structure.transform_values { |v| deep_dup(v) }
+        obj.transform_values { |v| deep_dup(v) }
       else
-        structure.dup
+        obj.dup
       end
+    end
+
+    def deep_merge(hash1, hash2)
+      hash = deep_dup(hash1)
+      hash2.each { |k, v| hash[k] = v }
+      hash
+    end
+
+    def deep_equal?(obj1, obj2)
+      obj1.object_id == obj2.object_id
+    end
+
+    def deep_transform_keys(obj, &block)
+      case obj
+      when Hash
+        obj.each_with_object({}) do |(k, v), result|
+          result[yield(k)] = deep_transform_keys(v, &block)
+        end
+      when Array
+        obj.map { |e| deep_transform_keys(e, &block) }
+      else
+        obj
+      end
+    end
+
+    def deep_transform_values(obj, &block)
+      case obj
+      when Hash
+        obj.transform_values { |v| deep_transform_values(v, &block) }
+      when Array
+        obj.map { |e| deep_transform_values(e, &block) }
+      else
+        yield(obj)
+      end
+    end
+
+    def deep_stringify_keys(obj)
+      deep_transform_keys(obj, &:to_s)
+    end
+
+    def deep_stringify_values(obj)
+      deep_transform_values(obj, &:to_s)
     end
   end
 end
---
+++ spec/lib/deep_nest_spec.rb
@@ -0,0 +1,3 @@
+RSpec.describe "DeepNest" do
+  
+end
